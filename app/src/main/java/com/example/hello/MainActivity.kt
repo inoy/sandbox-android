fc4450f52a935c89511c86f48609cb95d4efff85
inoy <7m7110123@gmail.com>
2023-09-17 14:33:59 +0900

    refactor to use setOnXxxListener

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hello
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Date
@@ -10,10 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun btnCurrentClick(@Suppress("UNUSED_PARAMETER") v: View) {
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = Date().toString()
+        val btnCurrent = findViewById<Button>(R.id.btnCurrent)
+        btnCurrent.setOnClickListener(
+            object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    val txtResult = findViewById<TextView>(R.id.txtResult)
+                    txtResult.text = Date().toString()
+                }
+
+            }
+        )
     }
 }
