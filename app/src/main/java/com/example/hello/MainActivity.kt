f760f8f0b5a6451ae74b28d812a35e2db5b35d51
inoy <7m7110123@gmail.com>
2023-09-17 14:40:01 +0900

    refactor to use view binding

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.hello
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hello.databinding.ActivityMainBinding
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnCurrent = findViewById<Button>(R.id.btnCurrent)
-        btnCurrent.setOnClickListener {
+        binding.btnCurrent.setOnClickListener {
             val txtResult = findViewById<TextView>(R.id.txtResult)
             txtResult.text = Date().toString()
         }
