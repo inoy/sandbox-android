59e551a81c791566430cabfd1f73d116533e9d75
inoy <7m7110123@gmail.com>
2023-09-17 14:32:12 +0900

    add buttonClick handler

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hello
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnCurrentClick(@Suppress("UNUSED_PARAMETER") v: View) {
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        txtResult.text = Date().toString()
     }
 }
